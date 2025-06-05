7a1cdd2829ec29b38618a19d7a381eb01cbac88a
heejung-0326 <duqwlqdk@gmail.com>
2025-06-05 12:48:12 +0900

    Update BooksCreate.js

--- src/component/BooksCreate.js
+++ src/component/BooksCreate.js
@@ -25,7 +25,7 @@ export default function BooksCreate() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:9070/books', form)
+    axios.post('backend/books', form)
       .then(() => {
         alert('상품이 등록 완료되었습니다.');
         navigate('/books');
